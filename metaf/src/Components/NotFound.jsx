868ed203580edae5f68e8bc635057e140e4d82f1
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2024-12-07 21:01:49 +0530

    notfound

---
+++ metaf/src/Components/NotFound.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../Components/Navbar"
+function NotFound() {
+    return <div>
+        <Navbar />
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exist!</p>
+    </div>
+}
+
+export default NotFound
